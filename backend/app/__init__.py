e4a53edeead3478b7ca80ca369ed5ce14e7b7ce3
Christian <christabanao331@gmail.com>
2026-09-03 23:30:18 +0800

    chore: Configure app and database migrations
            Added application configuration through config.py and updated __init__.py to integrate the configuration and Flaks-Migrate/Alembic database migration setup.Furthermore, updated the requirements.txt to include Flask-Migrate and Alembic for database migration management.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,23 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
 from .routes.typing import typing
-import os
 from dotenv import load_dotenv
-from app.extension import db
+from app.extension import db, migrate
 import app.models
+
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config.from_object("config.Config")
+
     CORS(app)
 
     app.register_blueprint(typing)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
+    migrate.init_app(app, db)
+   
     return app
